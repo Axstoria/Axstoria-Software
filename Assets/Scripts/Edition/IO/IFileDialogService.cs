a5cfd0205bfc7bb1fe6f1fb6c1af0ee52712c88b
Clément <clement.monnoire.1@ulaval.ca>
2026-04-11 14:02:43 -0400

    FIX: files repartition

--- Assets/Scripts/Edition/IO/IFileDialogService.cs
+++
@@ -1,9 +0,0 @@
-namespace Edition.IO
-{
-    public interface IFileDialogService
-    {
-        string SaveFile(string title, string defaultName, string extension); // returns full path or null
-        string OpenFile(string title, string extension);                     // returns full path or null
-        string OpenFile(string title, string[] extension);
-    }
-}
